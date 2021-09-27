ff310ab1ce6a36708e00b516564ee8a46e52d1ae
Werner Strydom <bloudraak@users.noreply.github.com>
2021-09-27 12:07:05 -0700

    Adds Team Rules

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "okta_group" "teams" {
   name        = "Department - ${each.value.name}" 
   description = each.value.description
 }
+
+resource "okta_group_rule" "teams" {
+    for_each    = local.teams
+  name              = "Department - ${each.value.name}" 
+  status            = "ACTIVE"
+  group_assignments = [okta_group.teams[each.key].id]
+  expression_value  = "user.department==\"${each.value.name}\""
+}
