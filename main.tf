45ae877831b6ba29f2323655e4b2a2c1c9d7df43
Werner Strydom <bloudraak@users.noreply.github.com>
2021-09-27 11:56:03 -0700

    Adds teams

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
-data "okta_everyone_group" "everyone" {
-    
+resource "okta_group" "teams" {
+  for_each    = local.teams
+  name        = "Department - ${each.value.name}" 
+  description = each.value.description
 }
