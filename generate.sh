98dc0336dbdcd33f8155415d64a2af57e1db8fbc
Werner Strydom <bloudraak@users.noreply.github.com>
2021-09-27 11:47:35 -0700

    Update Path

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ cat > backend.tf << EOL
 terraform {
   backend "s3" {
     bucket         = "${TF_BACKEND_S3_BUCKET}"
-    key            = "aws/v1.0.0/terraform.tfstate"
+    key            = "okta-entitlements/v1.0.0/terraform.tfstate"
     region         = "${TF_BACKEND_S3_REGION}"
     dynamodb_table = "${TF_BACKEND_S3_DYNAMODB_TABLE}"
     encrypt        = true
